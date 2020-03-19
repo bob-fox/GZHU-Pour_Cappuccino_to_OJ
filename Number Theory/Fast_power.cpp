d4251b356880b695f4a92639671284fdbdf5dde7
waltz26 <qq1932048447@163.com>
2020-03-19 12:56:31 +0800

    快速幂

---
+++ Number Theory/Fast_power.cpp
@@ -0,0 +1,14 @@
+/*--waltz26--*/
+
+long long fast_power(int a,int b,int mod)
+{
+    long long ans=1,base=a;
+    while(b)
+    {
+        if(b&1)
+            ans=ans*base%mod;
+        base=base*base%mod;
+        b>>=1;
+    }
+    return ans%mod;    //非多余步，如果没有b=0这种数据可以去掉取余
+}

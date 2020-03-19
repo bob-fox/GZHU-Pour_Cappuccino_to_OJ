5228514df40c21dfc11a1284153a9d54231d74d9
waltz26 <qq1932048447@163.com>
2020-03-19 12:18:12 +0800

    单向链表

---
+++ Data Structure/One-way_linkedList.cpp
@@ -0,0 +1,46 @@
+/*--waltz26--*/
+
+struct node
+{
+    int key;
+    node* rt;
+    node(int _key,node* _rt)    //构造函数
+    {
+        key=_key;
+        rt=_rt;
+    }
+};
+node *root=new node(-1,NULL);    //root是一个假结点
+int find_key(int key)
+{
+    for(node* p=root->rt;p;p=p->rt)
+        if(p->key==key) return 1;
+    return 0;
+}
+void insert_key(int key,int pos)    //pos输入合法
+{
+    node* p=root;
+    while (--pos)
+        p=p->rt;
+    node* target=new node(key,p->rt);
+    p->rt=target;
+}
+void delete_key(int pos)    //pos输入合法
+{
+    node* p=root;
+    while (--pos)
+        p=p->rt;
+    node* target=p->rt;
+    p->rt=target->rt;
+    delete target;    //释放节点
+}
+void go_list()    //遍历链表
+{
+    for(node* p=root->rt; p; p=p->rt)
+        printf("%d\n", p->key);
+}
+void debug()    //检查运行是否正确
+{
+    for(node* p=root->rt; p; p=p->rt)
+        printf("Node[%p] key=%d rt=%p\n", p, p->key, p->rt);
+}

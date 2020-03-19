9a1a8707a24db6e58022c17aa31ce111807fb218
waltz26 <qq1932048447@163.com>
2020-03-19 12:08:02 +0800

    单调栈

---
+++ Data Structure/Monotonic_stack.cpp
@@ -0,0 +1,23 @@
+/*--waltz26--*/
+
+struct node
+{
+    int h,i;
+};
+stack<node> s;
+int op[2];
+void monotonic_stack()
+{
+    for (int i=1;i<=n;i++)
+    {
+        cin>>h;
+        int pd=0,pi;
+        while (!s.empty())
+        {
+            if (s.top().h>a[i]) {pd=1; op[1]=s.top().i; break;}
+            else s.pop();
+        }
+        s.push((node){h,i});
+        cout<<op[pd];
+    }
+}

a655406ce42ba1ea93fbc2238a0b7a6d43d8298f
waltz26 <qq1932048447@163.com>
2020-03-26 21:31:57 +0800

    并查集

--- Data Structure/Union-find_sets.cpp
+++ Data Structure/Union-find_sets.cpp
@@ -1,7 +1,8 @@
 /*--waltz26--*/
 
-int fa[maxn]    //存储祖先
-int connected[maxn]={1};    //以下标为祖先的联通数
+
+int fa[100005];    //存储祖先
+int connected[100005];    //以下标为祖先的联通数，在main函数中将其初始化为1
 int ans;    //存储最大联通数
 
 int check_anc(int x)    //寻找祖先
@@ -20,7 +21,7 @@ bool is_same_anc(int x,int y)    //是否有相同祖先
     if (check_anc(x)==check_anc(y)) return 1;
     else return 0;
 }
-void union_anc_2(int x,int y)    //合并和联通并计算最大联通数
+void union_anc_2(int x,int y)    //合并并且计算最大联通数
 {
     x=check_anc(x); y=check_anc(y);
     if (x!=y)

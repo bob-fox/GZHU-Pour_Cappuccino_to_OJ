86d47f2b3ac0366707c9f9fed700ff0aa94c449b
waltz26 <qq1932048447@163.com>
2020-03-28 00:39:46 +0800

    Floyd算法

--- Graph Theory/Floyd.cpp
+++ Graph Theory/Floyd.cpp
@@ -7,9 +7,9 @@ int f[maxn][maxn][maxn];
 int main()
 {
     //前面记得初始化
-    for (int k=1;k<=n;k++)    // 注意顺序小心翻车  
-		for (int i=1;i<=n;i++)  
-	    	for (int j=1;j<=n;j++)  
+    for (int k=1;k<=n;k++)    // 注意顺序小心翻车
+		for (int i=1;i<=n;i++)
+	    	for (int j=1;j<=n;j++)
 				f[k][i][j]=min(f[k-1][i][j],f[k-1][i][k]+f[k-1][k][j]);
 }
 
@@ -18,13 +18,13 @@ int main()
 
 
 //空间优化
-int f[maxn][maxn][maxn];
+int f[maxn][maxn];
 
 int main()
 {
     //前面记得初始化
-    for (int k=1;k<=n;k++)    // 注意顺序小心翻车  
-		for (int i=1;i<=n;i++)  
-	    	for (int j=1;j<=n;j++)  
-				f[i][j]=min(f[[i][j],f[i][k]+f[k][j]);
+    for (int k=1;k<=n;k++)    // 注意顺序小心翻车
+		for (int i=1;i<=n;i++)
+	    	for (int j=1;j<=n;j++)
+				f[i][j]=min(f[i][j],f[i][k]+f[k][j]);
 }

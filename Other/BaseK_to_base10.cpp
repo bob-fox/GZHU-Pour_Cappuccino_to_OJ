828dcdb8fcb43c61394234a657c602083e862fe9
waltz26 <qq1932048447@163.com>
2020-03-28 01:12:50 +0800

    K进制转十进制

--- Other/BaseK_to_base10.cpp
+++ Other/BaseK_to_base10.cpp
@@ -1,13 +1,16 @@
 /*--waltz26--*/
 
-long long base10_to_basek(string s,int base)
+
+long long basek_to_base10(string s,int base)
 {
     long long ans=0;
     for(int i=0;i<s.size();i++)
     {
 	    char t=s[i];
-	    if(t>='0'&&t<='9') ans=ans*base+t-'0';
-	    else ans=ans*base+t-'A'+10;
+	    if(t>='0'&&t<='9') 
+            ans=ans*base+t-'0';
+	    else 
+            ans=ans*base+t-'A'+10;
     }
     return ans;
 }

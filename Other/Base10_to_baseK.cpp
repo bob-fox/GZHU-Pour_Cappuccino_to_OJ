e8830307f1a985618baaf37c3091bcd3f892b3d5
waltz26 <qq1932048447@163.com>
2020-03-28 01:11:09 +0800

    十进制转K进制

--- Other/Base10_to_baseK.cpp
+++ Other/Base10_to_baseK.cpp
@@ -1,15 +1,18 @@
 /*--waltz26--*/
 
+
 string base10_to_basek(long long n,int base)   
 {
     string ans="";
     do
     {
 	    int t=n%base;
-	    if(t>=0&&t<=9)	ans+=t+'0';
-	    else ans+=t-10+'A';
+	    if(t>=0&&t<=9)	
+            ans+=t+'0';
+	    else 
+            ans+=t-10+'A';
 	    n/=base;
-    } while(n!=0);	
-    reverse(ans.begin(),ans.end());
+    }while (n!=0);
+    reverse(INS(ans));
     return ans;
 }

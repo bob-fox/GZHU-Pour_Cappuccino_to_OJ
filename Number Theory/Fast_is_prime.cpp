47a68ba33a8b80a5b9f4d9df1b2be91fe7944de5
waltz26 <qq1932048447@163.com>
2020-03-28 00:57:05 +0800

    快速判断质数

--- Number Theory/Fast_is_prime.cpp
+++ Number Theory/Fast_is_prime.cpp
@@ -1,12 +1,14 @@
 /*--waltz26--*/
 
+
 bool fast_is_prime(int n)
 {
     if (n==1) return 0;
     if (n==2||n==3)
 	    return 1;
     if (n%6!=1&&n%6!=5)
-	return 0;
+	    return 0;
+
     for (int i=5;i*i<=n;i+=6)
 	    if (n%i==0||n%(i+2)==0)
 	        return 0;

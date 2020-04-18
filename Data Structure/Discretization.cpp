982f02745721f7f4593cd92c251f0724f1ee4a82
waltz26 <60854668+waltz26@users.noreply.github.com>
2020-04-18 19:45:00 +0800

    离散化

--- Data Structure/Discretization.cpp
+++ Data Structure/Discretization.cpp
@@ -9,16 +9,16 @@ int main()
     int n;
     cin>>n;
 
-	for (int i=1;i<=n;i++)
-		b[i]=a[i];
+    for (int i=1;i<=n;i++)
+	b[i]=a[i];
 
-	sort(b+1,b+n+1);
-	int m=unique(b+1,b+n+1)-(b+1);
+    sort(b+1,b+n+1);
+    int m=unique(b+1,b+n+1)-(b+1);
 
-	for (int i=1;i<=n;i++) 
+    for (int i=1;i<=n;i++) 
     {
-		int v=a[i];
-		a[i]=lower_bound(b+1,b+m+1,v)-b;
-		c[a[i]]=v;
-	}
+	int v=a[i];
+	a[i]=lower_bound(b+1,b+m+1,v)-b;
+	c[a[i]]=v;
+    }
 }

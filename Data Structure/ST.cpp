181338f0cf304dac5fece96c7e77018f8602cfd7
waltz26 <60854668+waltz26@users.noreply.github.com>
2020-04-23 23:14:43 +0800

    ST表

--- Data Structure/ST.cpp
+++ Data Structure/ST.cpp
@@ -1,19 +1,27 @@
 /*--waltz26--*/
 
 
-//最小值
+//ST表-最小值应用
 int f[20][100005];
-int a[100005];
+int lg2[100005];
 
 int main() 
 {
     int n;
     cin>>n;
+	
+    int t=0;
+    for (int i=1;i<=n;i++)    //初始化log2
+    {
+        if (i>=(1<<(t+1))) t++;
+        lg2[i]=t;
+    }
+	
     for (int i=1;i<=n;i++)
-    cin>>f[0][i];
+    	cin>>f[0][i];
 
     for (int j=1;j<17;j++)
-        for (int i=1;i<=n;i++)
+        for (int i=1;i+(1<<(j-1))<=n;i++)
 	    f[j][i]=min(f[j-1][i],f[j-1][i+(1<<(j-1))]);
 
     int l,r;

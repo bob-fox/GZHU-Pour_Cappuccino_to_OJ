a2b7d2ba3085ee6e4fbc21e20f01e5fd104ed94b
waltz26 <60854668+waltz26@users.noreply.github.com>
2020-04-24 23:55:35 +0800

    离散化

--- Data Structure/Discretization.cpp
+++ Data Structure/Discretization.cpp
@@ -1,7 +1,7 @@
 /*--waltz26--*/
 
 
-//基础
+//a为原数组（离散化数组），b为离散化对照数组，c为映射数组
 int a[1000005],b[1000005],c[1000005];
 
 int main() 
@@ -10,7 +10,10 @@ int main()
     cin>>n;
 
     for (int i=1;i<=n;i++)
+    {
+        cin>>a[i];
 	b[i]=a[i];
+    }
 
     sort(b+1,b+n+1);
     int m=unique(b+1,b+n+1)-(b+1);
@@ -19,6 +22,6 @@ int main()
     {
 	int v=a[i];
 	a[i]=lower_bound(b+1,b+m+1,v)-b;
-	c[a[i]]=v;
+	c[a[i]]+=v;
     }
 }

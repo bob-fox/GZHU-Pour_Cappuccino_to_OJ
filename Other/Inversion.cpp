8b3c25a3949750b48245711b8e5183f4751c3e4f
waltz26 <60854668+waltz26@users.noreply.github.com>
2020-04-29 23:31:48 +0800

    逆序对

--- Other/Inversion.cpp
+++ Other/Inversion.cpp
@@ -25,3 +25,61 @@ void msort(int l,int r)
     for(int i=l;i<=r;i++) 
         a[i]=p[i];
 }
+
+
+/*--waltz26--*/
+
+
+//树状数组求逆序对
+int a[500005],bit[500005],r[500005];
+long long ans;
+ 
+bool cmp(const int &x,const int &y)
+{
+    return a[x]<a[y];
+}
+ 
+int lowbit(int x)
+{
+    //算出x二进制的从右往左出现第一个1以及这个1之后的那些0组成数的二进制对应的十进制的数
+    return x&-x;
+}
+void change_point(int u,int d,int j)    //单点修改
+{
+    while (u<=j)    //不能越界
+    {
+        bit[u]+=d;
+        u+=lowbit(u);
+    }
+}
+int ask_sum(int u)    //区间查询
+{
+    int ans=0;
+    while (u>=1)
+    {
+        ans+=bit[u];
+        u-=lowbit(u);
+    }
+    return ans;
+}
+ 
+int main()
+{
+    int n;
+    cin>>n;
+ 
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        r[i]=i;
+    }
+    sort(r+1,r+1+n,cmp);
+ 
+    for(int i=1;i<=n;i++)
+    {
+        change_point(r[i],1,n);
+        ans+=i-ask_sum(r[i]);
+    }
+    cout<<ans<<'\n';
+    return 0;
+}

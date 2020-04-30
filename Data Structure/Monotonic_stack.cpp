45ca27db6e3102107cf93870e744359b80276aae
waltz26 <60854668+waltz26@users.noreply.github.com>
2020-04-30 17:48:07 +0800

    单调栈

--- Data Structure/Monotonic_stack.cpp
+++ Data Structure/Monotonic_stack.cpp
@@ -9,18 +9,24 @@ struct node
 stack<node> s;
 int op[2];
 
-void monotonic_stack()
+int main()
 {
+    int n;
+    cin>>n;
+    
     for (int i=1;i<=n;i++)
     {
+        int h;
         cin>>h;
-        int pd=0,pi;
+        
+        int pd=0;
         while (!s.empty())
         {
-            if (s.top().h>a[i]) {pd=1; op[1]=s.top().i; break;}
+            if (s.top().h>h) {pd=1; op[1]=s.top().i; break;}
             else s.pop();
         }
+
         s.push((node){h,i});
-        cout<<op[pd];
+        cout<<op[pd]<<' ';
     }
 }

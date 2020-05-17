aceb394a398db30c9c7bad9ba599905db520d285
waltz26 <qq1932048447@163.com>
2020-05-17 13:16:26 +0800

    子集生成

--- Enumerate/Print_subset.cpp
+++ Enumerate/Print_subset.cpp
@@ -16,3 +16,23 @@ int main()
     for (int i=0;i<(1<<n);i++)    //0为空集，这条循环遍历所有子集
 	    print_subset(n,i);
 }
+
+
+/*--waltz26--*/
+
+
+//位向量法
+void print_subset(int n,int cur)
+{
+    if (cur==n)
+    {
+        for (int i=0;i<cur;i++)
+            if (vec[i]) printf("%d ",i);
+        puts("");
+        return;
+    }
+    vec[cur]=1;    \\选第cur个元素
+    print_subset(n,cur+1);
+    vec[cur]=0;    \\不选第cur个元素
+    print_subset(n,cur+1);
+}
